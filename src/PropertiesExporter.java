3427cf0a6c9dfa7871a2df7f76ac378a9e9888d7
nitheeshvijayan156 <118504540+nitheeshvijayan156@users.noreply.github.com>
2023-05-10 09:56:45 +0530

    updates

---
+++ src/PropertiesExporter.java
@@ -0,0 +1,42 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class PropertiesExporter {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/pms";
+        String user = "root";
+        String password = "227069";
+        
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM properties")) {
+            
+            FileWriter writer = new FileWriter("properties.csv");
+            writer.write("id,address,rent,is_available,num_bedrooms,status\n");
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String address = rs.getString("address");
+                double rent = rs.getDouble("rent");
+                boolean isAvailable = rs.getBoolean("is_available");
+                int numBedrooms = rs.getInt("num_bedrooms");
+                String status = rs.getString("status");
+                
+                writer.write(id + "," + address + "," + rent + "," + isAvailable + "," + numBedrooms + "," + status + "\n");
+            }
+            
+            writer.close();
+            System.out.println("Data exported to properties.csv");
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
